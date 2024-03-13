0da89efba9db64f49ac93e505b30f01fd67cf21d
Sneha Lodha <sneha.lodha@ing.com>
2024-03-13 19:31:15 +0100

    consumption overview pages

--- app/frontend/progressive-web-app/src/App.js
+++ app/frontend/progressive-web-app/src/App.js
@@ -1,5 +1,6 @@
 import Scanner from "./components/scanner/Scanner";
 import ReceiptEdit from "./components/scanner/receiptEdit/ReceiptEdit";
+import ConsumptionOverview from "./components/consumption/ConsumptionOverview";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route index element={<Scanner />} />
         <Route path="contribute/:receiptId" element={<ReceiptEdit />} />
+        <Route path="consumption" element={<ConsumptionOverview />} />
       </Routes>
     </BrowserRouter>
   );
