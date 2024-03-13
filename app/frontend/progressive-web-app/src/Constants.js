55f266e57d6ec8c0e84bd18e0d4f60999d68b1dc
TychoZaal <TychoZaal@gmail.com>
2024-03-13 20:16:59 +0100

    color codes added

---
+++ app/frontend/progressive-web-app/src/Constants.js
@@ -0,0 +1,8 @@
+const COLORS = {
+    primaryOrange: '#F0F0F0',
+    primaryWhite: '#FFFFFF',
+    primaryBackground: '#F0F0F0',
+    primaryWebText: '#333333',
+    secondaryIndigo: '#525199',
+    regularWebText: '#696969'
+};
