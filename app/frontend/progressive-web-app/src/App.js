a54cd0a2e401d1ecb62c1ea633cf3bd54acff43f
TychoZaal <TychoZaal@gmail.com>
2024-03-14 09:23:18 +0100

    removed paths

--- app/frontend/progressive-web-app/src/App.js
+++ app/frontend/progressive-web-app/src/App.js
@@ -4,7 +4,6 @@ import ReceiptEdit from "./components/scanner/receiptEdit/ReceiptEdit";
 import ConsumptionOverview from "./components/consumption/ConsumptionOverview";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./globalVariables.css";
-import ShareLink from "./components/scanner/receiptEdit/ShareLink";
 
 function App() {
   return (
@@ -13,8 +12,6 @@ function App() {
         <Route index element={<Scanner />} />
         <Route path="edit/:receiptId" element={<ReceiptEdit />} />
         <Route path="contribute/:receiptId" element={<Settle />} />
-        <Route path="share" element={<ShareLink />} />
-        <Route path="contribute" element={<ReceiptEdit receiptData={{items: [{amount: 2, price: 10.25, name: "A", totalPrice: 20.50}]}}/>} />
       </Routes>
     </BrowserRouter>
   );
