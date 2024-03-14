4e79cc899ba9b5673133fddb366b2b8346e8ae0f
TychoZaal <TychoZaal@gmail.com>
2024-03-14 09:22:36 +0100

    Delete button & styling fixes

--- app/frontend/progressive-web-app/src/App.js
+++ app/frontend/progressive-web-app/src/App.js
@@ -4,6 +4,7 @@ import ReceiptEdit from "./components/scanner/receiptEdit/ReceiptEdit";
 import ConsumptionOverview from "./components/consumption/ConsumptionOverview";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./globalVariables.css";
+import ShareLink from "./components/scanner/receiptEdit/ShareLink";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route index element={<Scanner />} />
         <Route path="edit/:receiptId" element={<ReceiptEdit />} />
         <Route path="contribute/:receiptId" element={<Settle />} />
+        <Route path="share" element={<ShareLink />} />
+        <Route path="contribute" element={<ReceiptEdit receiptData={{items: [{amount: 2, price: 10.25, name: "A", totalPrice: 20.50}]}}/>} />
       </Routes>
     </BrowserRouter>
   );
