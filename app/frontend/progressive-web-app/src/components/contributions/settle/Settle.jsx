9ed4bbccf94c7e8ff728e68c7e4cd85fcdb2b5ac
Job Kanis <job.kanis@ing.com>
2024-03-14 07:47:23 +0100

    Retrieve items from backend

--- app/frontend/progressive-web-app/src/components/contributions/settle/Settle.jsx
+++ app/frontend/progressive-web-app/src/components/contributions/settle/Settle.jsx
@@ -1,14 +1,41 @@
 import React, { useEffect, useState } from "react";
 import './Settle.css';
 import ReceiptItem from "../receiptitem/ReceiptItem";
+import axios from "axios";
 
 function Settle() {
-    const receiptData = {
-        items: [
-            { name: "A", amount: 10, totalPrice: 12.5 },
-            { name: "B", amount: 5, totalPrice: 7.5 },
-        ],
-    };
+    const [bill_uuid, _] = useState("cf087d8a-7504-480e-a4ba-ccc873f5d1e7");
+    const [receiptData, setReceiptData] = useState([])
+    async function loadReceiptData() {
+        const res = await axios.get("http://localhost:8000/bill/"+bill_uuid+"/get");
+        const bill = JSON.parse(JSON.stringify(res.data.bill))
+        var receiptData = bill.items.map((item) => {
+            return {
+                id: item.id,
+                name: item.name,
+                amount: item.amount,
+                totalPrice: item.totalPrice
+            }
+        })
+        setReceiptData(receiptData)
+    }
+    
+    useEffect(() => {
+        loadReceiptData()
+    }, []);
+
+    async function put_request(path, json_data) {
+        var data = new FormData()
+        data.append("json", JSON.stringify(json_data))
+    
+        const response = await axios.put(path, JSON.stringify(json_data), {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
+    }
+
 
     const options = { forceIsCustomContribution: false };
 
@@ -28,8 +55,7 @@ function Settle() {
     return (
         <div className="receipt-container">
             <ul className="receipt-items">
-                {receiptData &&
-                    receiptData.items.map((item, index) => (
+                { receiptData.map((item, index) => (
                         <li key={index}>
                             <ReceiptItem
                                 item={item}
